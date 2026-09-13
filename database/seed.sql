ac99ee330a8afbff1cc3f906dc69f98acc051d1e
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-09-13 17:13:02 -0300

    chore: adicionar dados iniciais de esmaltes

---
+++ database/seed.sql
@@ -0,0 +1,80 @@
+INSERT INTO Esmaltes
+    (Nome, Marca, DataVencimento, Observacoes, CaminhoImagem)
+VALUES
+
+-- Dailus
+('cherry mocha', 'Dailus', '2026-06-01', NULL, NULL),
+('licor de amarena', 'Dailus', '2027-04-01', NULL, NULL),
+('tulip twist', 'Dailus', '2026-10-01', NULL, NULL),
+('chifre mágico', 'Dailus', '2028-03-01', NULL, NULL),
+('frozen lake', 'Dailus', '2026-02-01', NULL, NULL),
+('morango do amor', 'Dailus', '2027-10-01', NULL, NULL),
+
+-- Risqué
+('atração principal', 'Risque', '2028-11-01', NULL, NULL),
+('renda', 'Risque', '2028-02-01', NULL, NULL),
+('verdelirio', 'Risque', '2028-03-01', NULL, NULL),
+('palco cherry', 'Risque', '2028-11-01', NULL, NULL),
+('eu sou esse glamour todo', 'Risque', '2028-02-01', NULL, NULL),
+('foto no lambe lambe', 'Risque', '2028-11-01', NULL, NULL),
+('ver de verdade', 'Risque', '2027-03-01', NULL, NULL),
+('a mar', 'Risque', '2028-05-01', NULL, NULL),
+('as mil purpurinas', 'Risque', '2027-03-01', NULL, NULL),
+('granulado rose', 'Risque', '2027-03-01', NULL, NULL),
+('roxo sem crise', 'Risque', '2027-05-01', NULL, NULL),
+('chiclete, me esquece', 'Risque', '2027-06-01', NULL, NULL),
+('Leo mandou flores', 'Risque', '2027-04-01', NULL, NULL),
+('nada óbvio', 'Risque', '2029-04-01', NULL, NULL),
+('lavanda alma', 'Risque', '2027-04-01', 'quase acabando', NULL),
+('tule', 'Risque', '2027-03-01', 'quase acabando', NULL),
+('beijo', 'Risque', '2027-12-01', NULL, NULL),
+('ouro nude', 'Risque', '2027-04-01', 'quase acabando', NULL),
+('Gabriela', 'Risque', '2029-04-01', NULL, NULL),
+('Rose bombom', 'Risque', '2027-10-01', NULL, NULL),
+('base e cobertura fosca efeito matte', 'Risque', '2026-10-01', NULL, NULL),
+('menta.liza', 'Risque', '2026-09-01', NULL, NULL),
+('antes sol que mal iluminada', 'Risque', '2026-04-01', NULL, NULL),
+('guerreira do arco íris', 'Risque', '2026-06-01', NULL, NULL),
+('azulivre mente', 'Risque', '2025-11-01', NULL, NULL),
+('bali', 'Risque', '2026-11-01', NULL, NULL),
+('me poupe, mas quem me dera', 'Risque', '2025-07-01', NULL, NULL),
+('cappuccino', 'Risque', '2026-05-01', NULL, NULL),
+('energia', 'Risque', '2026-11-01', NULL, NULL),
+('bela e poderosa', 'Risque', '2026-12-01', NULL, NULL),
+('chóque pink', 'Risque', '2026-10-01', NULL, NULL),
+
+-- Colorama
+('barbie boss', 'Colorama', '2028-07-01', NULL, NULL),
+('barbie girl', 'Colorama', '2028-06-01', NULL, NULL),
+
+-- Impala
+('casco de cavalo base endurecedor', 'Impala', '2029-04-01', NULL, NULL),
+('asa de fada', 'Impala', '2029-04-01', NULL, NULL),
+('base nutritiva', 'Impala', '2029-04-01', NULL, NULL),
+('ostente seu poder', 'Impala', '2027-07-01', NULL, NULL),
+('zaz', 'Impala', '2026-01-01', NULL, NULL),
+('amor profundo', 'Impala', '2026-11-01', NULL, NULL),
+('na ponta do pé', 'Impala', '2025-12-01', NULL, NULL),
+('verniz extra brilho', 'Impala', '2029-03-01', NULL, NULL),
+('xodó', 'Impala', '2026-11-01', 'quase acabando', NULL),
+
+-- Drica
+('café', 'Drica', '2026-12-01', NULL, NULL),
+('glitter flash', 'Drica', '2025-12-01', NULL, NULL),
+('perolado Paris', 'Drica', '2026-12-01', NULL, NULL),
+('Amélia', 'Drica', '2026-12-01', NULL, NULL),
+('glitter Flávia', 'Drica', '2026-01-01', NULL, NULL),
+('casco de tatu', 'Drica', '2025-08-01', NULL, NULL),
+('aries', 'Drica', '2026-12-01', NULL, NULL),
+('Roma', 'Drica', '2027-03-01', NULL, NULL),
+
+-- Hits 5Free
+('blueberry jelly', 'Hits 5Free', '2026-01-01', NULL, NULL),
+('diamante verde aurora', 'Hits 5Free', '2026-12-01', NULL, NULL),
+
+-- Top Beauty
+('flash', 'Top Beauty', NULL, 'não tem data escrita', NULL),
+
+-- Avon
+('prata glitter', 'Avon', '2026-11-01', NULL, NULL),
+('quase para sempre', 'Avon', '2026-01-01', NULL, NULL);
